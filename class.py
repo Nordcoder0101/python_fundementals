0982780b0db25d0a1e1465d9445c9a227e191a34
Greg <greg@Gregs-MacBook-Air.local>
2018-10-30 08:45:06 -0700

    chaining added

--- class.py
+++ class.py
@@ -5,40 +5,31 @@ class User:
         self.account_balance = 0
     def makeDeposit(self, amount):
         self.account_balance += amount
+        return self
 
     def makeWithdrawl(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(self.account_balance)
+        return self
 
     def transfer_money(self, amount, recipiant):
         self.account_balance -= amount
         recipiant.account_balance += amount  
-
+        return self
+        
 greg = User("Greg", "nords3x4@gmail.com")
 joe = User("Joe", "joe@joeshmo.com")
 ned = User("Ned", "nedisneat@ned.com")
 
 
-greg.makeDeposit(10000)
-greg.makeDeposit(10000)
-greg.makeDeposit(10000)
-greg.makeWithdrawl(50)
-greg.display_user_balance()
+greg.makeDeposit(10000).makeDeposit(10000).makeDeposit(10000).makeWithdrawl(50).display_user_balance()
 
-joe.makeDeposit(50)
-joe.makeDeposit(50)
-joe.makeWithdrawl(10)
-joe.makeWithdrawl(10)
-joe.display_user_balance()
+joe.makeDeposit(50).makeDeposit(50).makeWithdrawl(10).makeWithdrawl(10).display_user_balance()
 
-ned.makeDeposit(100)
-ned.makeWithdrawl(10)
-ned.makeWithdrawl(10)
-ned.makeWithdrawl(10)
-ned.display_user_balance()
+ned.makeDeposit(100).makeWithdrawl(10).makeWithdrawl(10).makeWithdrawl(10).display_user_balance()
 
-greg.transfer_money(20, ned)
-greg.display_user_balance()
+greg.transfer_money(20, ned).display_user_balance()
 ned.display_user_balance()
